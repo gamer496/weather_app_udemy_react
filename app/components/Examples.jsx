af662a9cfe9cfe5e89ad8d70119a108b21b019bc
gamer496 <singhjasdeep496@gmail.com>
2017-01-21 16:08:55 +0530

    linking of search box remains

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 
 var Examples = (props) => {
-  return <h3>Examples</h3>;
+  return (
+    <div>
+      <h1 className = "text-centered">Examples</h1>
+      <p>Here are a few example locations to try out</p>
+      <ol>
+        <li>
+          <Link to = "/?location=Delhi">Delhi</Link>
+        </li>
+        <li>
+          <Link to="/?location=Vostok">Vostok</Link>
+        </li>
+      </ol>
+    </div>
+  );
 }
 
 module.exports = Examples;
